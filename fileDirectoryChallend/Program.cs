b374ffc59cf6dabb232de0ed56b54aab9268a242
madrow1 <rowan.pitt@btinternet.com>
2023-12-08 15:52:17 +0000

    Files

---
+++ fileDirectoryChallend/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int totalFiles = 0;
+
+        const string filename = "results.txt";
+
+        string workingDir = Directory.GetCurrentDirectory();
+
+        string[] files = Directory.GetFiles(workingDir);
+
+        foreach (string f in files)
+        {
+            totalFiles++;
+        }
+
+        if (File.Exists("results.txt"))
+        {
+            using(StreamWriter sw = File.AppendText($"Total Number of Files: {totalFiles}"));
+        }
+        else
+        {
+            using(StreamWriter sw = File.CreateText(filename))
+            {
+                sw.WriteLine($"Total Number of Files: {totalFiles}");
+            }
+        }
+
+    }
+}
---
+++ files/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+const string filename = "TestFile.txt";
+
+//using(StreamWriter sw = File.CreateText(filename)) {
+//   sw.WriteLine("This is a text file.");
+//};
+
+Console.WriteLine(File.Exists(filename));
+
+if(File.Exists(filename)) {
+    File.Delete(filename);
+}
+else {
+    using(StreamWriter sw = File.CreateText(filename)) {
+        sw.WriteLine("This is a text file.");
+    }
+}
+
+//if (!File.Exists(filename)) {
+//    File.WriteAllText(filename, "This is a text file.");
+//}
+
+//string content;
+
+//content = File.ReadAllText(filename);
+//Console.WriteLine(content);
+
+Console.WriteLine(File.GetCreationTime(filename));
+Console.WriteLine(File.GetLastWriteTime(filename));
+Console.WriteLine(File.GetLastAccessTime(filename));
+
+File.SetAttributes(filename, FileAttributes.ReadOnly);
+Console.WriteLine(File.GetAttributes(filename));
